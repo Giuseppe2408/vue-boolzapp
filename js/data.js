452bb5dbb153af8c94391b6b55ff462451b1004c
Giuseppe <giuseppe.acito.ga@gmail.com>
2022-05-30 15:59:05 +0200

    milestone 3

--- js/data.js
+++ js/data.js
@@ -3,6 +3,7 @@ const myContacts = new Vue({
 
     data : {
         activeContacts : 0,
+        newMsg : '',
         contacts : [
             {
                 name: 'Michele',
@@ -181,6 +182,18 @@ const myContacts = new Vue({
             this.activesContacts++;
             this.activeContacts = activeContacts;
             console.log(activeContacts);
+        },
+
+        newMessage(){
+            const newMsg = {
+                date : '30/05/2022 15:48',
+                message : this.newMsg,
+                status : 'sent',
+            }
+            if (this.newMsg !== "") {
+                this.contacts.messages.push(newMsg);
+                this.newMsg = "";
+            }      
         }
     }
 }); 
